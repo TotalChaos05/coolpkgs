d45bbfdefaa58e7ca74975ed24f7010ab15420b0
Basil Keeler <basil.keeler@outlook.com>
2023-12-18 23:42:50 -0600

    remove a thing

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
-  bitwig = pkgs.callPackage ./pkgs/bitwig {};
+  # bitwig = pkgs.callPackage ./pkgs/bitwig {};
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
